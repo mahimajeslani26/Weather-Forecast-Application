0c838a320ef530c34f51bed19f7e33408946a3df
System Administrator <root@Prernas-MacBook-Pro.local>
2021-06-12 23:36:06 +0530

    Port heroku issue fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast');
 
 const app = express();
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
