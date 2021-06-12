9c56d8e55f5a61f11aa4919b941a0e07bf9e2c0a
System Administrator <root@Prernas-MacBook-Pro.local>
2021-06-12 21:16:19 +0530

    Add API access tokens

---
+++ access_tokens.js
@@ -0,0 +1,8 @@
+const mapbox_access_token =
+  'pk.eyJ1IjoibWplczEyMyIsImEiOiJjajZ0ZGNmYmwwZ2EyMzhvMzBnMG1ubTZnIn0.rRal0x_beWhKtmOEZahvww';
+
+const weatherstack_access_key = '07c08db3a5f6c6926283f105b147b424';
+
+module.exports.mapbox_access_token = mapbox_access_token;
+
+module.exports.weatherstack_access_key = weatherstack_access_key;
